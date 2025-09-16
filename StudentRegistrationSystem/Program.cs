8ef8da96e4f0839292b722646bf61662a8ffd72d
Enver Ismaili <ismailienver2002@gmail.com>
2025-09-16 22:38:13 +0200

    cm

--- StudentRegistrationSystem/Program.cs
+++ StudentRegistrationSystem/Program.cs
@@ -21,6 +21,12 @@ builder.Services.AddAutoMapper(typeof(StudentMapper));
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<StudentDbContext>();
+    db.Database.Migrate();
+}
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
